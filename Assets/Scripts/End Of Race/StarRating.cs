422d0b8fa6d70895d0ae0a2c506180f2c8ffa81b
Isaac - PC <IsaacTFagg@gmail.com>
2020-01-21 15:02:34 +0000

    More papers + rating page

---
+++ Assets/Scripts/End Of Race/StarRating.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarRating : MonoBehaviour
+{
+	public int score;
+
+	public Button[] stars;
+
+	public Button star1;
+	public Button star2;
+	public Button star3;
+	public Button star4;
+	public Button star5;
+
+	//Select 1 button, selects all buttons before it
+
+	//Deselecting only does 1 deselect
+
+	//outputs score
+
+	public int GenerateScore()
+	{
+		foreach (Button star in stars)
+		{
+			if (star.spriteState = )
+			{
+
+			}
+		}
+	}
+
+
+}
