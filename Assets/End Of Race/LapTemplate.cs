9078568115915f43b4234e4b3071e2aac320ac10
Isaac - PC <IsaacTFagg@gmail.com>
2020-01-23 18:25:26 +0000

    Stars, lap times and requests

---
+++ Assets/End Of Race/LapTemplate.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTemplate : MonoBehaviour
+{
+	public int lapNumber;
+	public float lapTime;
+	public enum LapTimeType { Standard, Fastest, Total}
+	public LapTimeType lapType;
+
+	public Text lapNumberText;
+	public Text lapTimeText;
+
+	private void Start()
+	{
+		if (!lapNumberText)
+		{
+			lapNumberText = gameObject.transform.GetChild(0).GetComponent<Text>();
+		}
+		if (lapTimeText)
+		{
+			lapTimeText = gameObject.transform.GetChild(1).GetComponent<Text>();
+		}
+
+		PopulateText();
+	}
+
+	public void PopulateText()
+	{
+		if (lapType == LapTimeType.Total)
+		{
+			lapNumberText.text = ("Total: ");
+		}
+		else if (lapType == LapTimeType.Fastest)
+		{
+			lapNumberText.text = ("Fastest Lap: ");
+
+		}
+		else
+		{
+			lapNumberText.text = ("Lap " + lapNumber + ": ");
+
+		}
+
+
+		lapTimeText.text = FormatLapTime(lapTime);
+	}
+
+	public string FormatLapTime(float time)
+	{
+		int minutes = (int)time / 60;
+		int seconds = (int)time % 60;
+		float milliseconds = time * 100;
+		milliseconds %= 100;
+
+		return string.Format("{0:0}:{1:00}:{2:00}", minutes, seconds, milliseconds);
+	}
+}
---
+++ Assets/End Of Race/LapTimes.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTimes : MonoBehaviour
+{
+	public GameObject lapTemplate;
+
+	public ScrollRect scrollView;
+	public GameObject contentParent;
+
+	public LapTemplate fastestLap;
+	public LapTemplate totalLap;
+
+	public List<float> lapTimes;
+
+	private void Start()
+	{
+		PopulateScrollView();
+
+		SetFastestLap();
+
+		TotalLapTime();
+	}
+
+
+	public void PopulateScrollView()
+	{
+		float tempHeight = lapTemplate.GetComponent<RectTransform>().rect.height;
+		float totalHeight = (lapTimes.Count * tempHeight);
+
+		//Debug.Log(totalHeight);
+
+		RectTransform contentRect = contentParent.GetComponent<RectTransform>();
+		Vector2 newSize = new Vector2(contentRect.rect.x, totalHeight);
+		contentRect.sizeDelta = newSize;
+
+
+
+
+		for (int i = 0; i < lapTimes.Count; i++)
+		{
+			GameObject newLap = Instantiate(lapTemplate, contentParent.transform);
+
+			LapTemplate newLapInfo = newLap.GetComponent<LapTemplate>();
+			newLapInfo.lapTime = lapTimes[i];
+			newLapInfo.lapNumber = i + 1;
+			newLapInfo.PopulateText();
+
+
+
+		}
+
+	}
+
+	public void SetFastestLap()
+	{
+		fastestLap.lapTime = lapTimes.Min();
+		fastestLap.PopulateText();
+	}
+
+	public void TotalLapTime()
+	{
+		float total = 0;
+
+		foreach (float lap in lapTimes)
+		{
+			total += lap;
+		}
+
+		totalLap.lapTime = total;
+
+		totalLap.PopulateText();
+	}
+
+}
